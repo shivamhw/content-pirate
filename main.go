4801b73374ef59ae92c7e5b4a52c10d8251b14e8
Shivam <shivamhw0@gmail.com>
2025-06-19 18:50:41 +0530

    removed comments

--- main.go
+++ main.go
@@ -6,15 +6,4 @@ import (
 
 func main() {
 	cmd.Execute()
-	// // tdl.Code(context.Background())
-	// ctx := context.Background()
-	// t := telegram.NewTelegram(ctx)
-	// user :=  &telegram.UserData{PhoneNumber: "+918085026377"}
-	// t.Login(user, false)
-	// // t.ListChats(&telegram.UserData{PhoneNumber: "+918085026377"})
-	// // t.ExportChat(&telegram.UserData{PhoneNumber: "+918085026377",}, telegram.ExportOpts{
-	// // 	ChatId: "1237061921",
-	// // 	Limit: 100,
-	// // })
-	// // t.DownloadExport(user, telegram.DownloadOpts{})
 }
