1d34b7761d0e944f28eaff37b33a97bbcf9a683c
shivamhw <s.kmishra703@gmail.com>
2025-06-24 23:36:47 +0530

    updated redditfilter; downloads now support skip-same

--- cmd/scrape.go
+++ cmd/scrape.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/shivamhw/content-pirate/pkg/reddit"
 	"github.com/shivamhw/content-pirate/pkg/scrapper"
 	"github.com/shivamhw/content-pirate/sources"
 	"github.com/shivamhw/content-pirate/store"
@@ -18,11 +20,17 @@ var (
 
 func scrapeCmd() *cobra.Command {
 	var jIds []string
+	var filter string
 	cmd := &cobra.Command{
 		Use:   "scrape",
 		Long:  "Scrapes subreddit for videos and imgs",
 		Short: "scrapes subreddit",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			f1, err := sanitizeFilter(&filter)
+			if err != nil {
+				return err
+			}
+			scrapeOpts.RedditFilter = f1
 			s, err := scrapper.NewScrapper(&sCfg)
 			if err != nil {
 				return err
@@ -64,10 +72,20 @@ func scrapeCmd() *cobra.Command {
 	cmd.Flags().BoolVar(&scrapeOpts.SkipVideos, "skip-vid", true, "skip video download")
 	cmd.Flags().BoolVar(&dst.CombineDir, "combine", false, "combine folders")
 	cmd.Flags().BoolVar(&scrapeOpts.SkipCollection, "skip-collection", false, "download full collection")
-	cmd.Flags().BoolVar(&dst.CleanOnStart, "cleanOnStart", true, "clean folders")
+	cmd.Flags().BoolVar(&dst.CleanOnStart, "cleanOnStart", false, "clean folders")
 	cmd.Flags().IntVar(&sCfg.ImgWorkers, "img-worker", 10, "nof img proccesing worker")
 	cmd.Flags().IntVar(&sCfg.VidWorkers, "vid-worker", 5, "nof vid proccesing worker")
 	cmd.Flags().IntVar(&sCfg.TopicWorkers, "reddit-worker", 15, "nof reddit proccesing worker")
-
+	cmd.Flags().StringVar(&filter, "filter", "TOP", "filter: NEW, HOT, TOP")
 	return cmd
+}
+
+
+func sanitizeFilter(f *string) (reddit.PostFilter, error) {
+	filter := strings.ToUpper(fmt.Sprintf("REDDIT_%s", *f))
+	f1 := reddit.PostFilter(filter)
+	if f1 != reddit.REDDIT_HOT && f1 != reddit.REDDIT_NEW && f1 != reddit.REDDIT_TOP {
+		return reddit.PostFilter(""), fmt.Errorf("unknown filter for reddit %s", f1)
+	}
+	return f1, nil
 }
